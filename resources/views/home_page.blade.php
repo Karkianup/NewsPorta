90f766113f175416f8ccfc51dd9eddb9c96074b6
unknown <karkianup134@gmail.com>
2022-01-14 14:34:10 +0545

    a

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -11,16 +11,20 @@
         <div class="row">
            <div class="col-5">
 
+<div class="card">
+<div class="card-body">
 
                 @foreach ($newsDetails as $d)
 
                         <a href={{ "/news/". $d->id }} style="text-decoration: none">
                           <h1 style="color:Black;font-weight:bold">{{ $d->title }}</h1> <br>
                           <img src={{"images/".$d->image  }} width="300px" height="200px"><br><br>
-                          <hr style="width:1200px;height:6px;">
+                          <hr style="width:430px;height:6px;">
                         </a>
 
                 @endforeach
+            </div>
+        </div>
 
            </div>
 
--- resources/views/show_news.blade.php
+++ resources/views/show_news.blade.php
@@ -3,19 +3,27 @@
    <div class="container addNews">
       <div class="row">
             <div class="col-8">
-                 @if(auth()->check())
-                     @if(auth()->user()->id==$newsDetails->user_id)
-                        <a href={{"/news/delete/".$newsDetails->id}} style="position: absolute;left:60%;font-size:22px;text-decoration:none" class="btn btn-danger">Delete your Post</a>
-                         <a href={{"/news/edit/".$newsDetails->id}} style="position: absolute;left:44%;font-size:22px;text-decoration:none" class="btn btn-warning">Edit Your Post </a><br><br><br>
-                      @endif
-                  @endif
-                    <h1 style="font-weight:bold;font-family:verdana;color:purple;text-decoration:underline;font-size:54px;">{{$newsDetails->title  }}</h1><br>
-                    <img src="{{asset('images/'.$newsDetails->image)}}" width="400px" height="200px" style="border-radius:30px"><br><br>
-                    <span style="color:black;font-weight:bold;font-size:24px">{{ $newsDetails->post }} </span><br><br>
 
-                    <span style="color:black;font-weight:bold;font-size:24px">Article by:</span><span style="font-weight: bold;color:seagreen;font-size:29px">{{ $newsDetails->user->name }}</span>
+                    @if(auth()->check())
+                        @if(auth()->user()->id==$newsDetails->user_id)
+                            <a href={{"/news/delete/".$newsDetails->id}} style="position: absolute;left:60%;font-size:22px;text-decoration:none" class="btn btn-danger">Delete your Post</a>
+                            <a href={{"/news/edit/".$newsDetails->id}} style="position: absolute;left:44%;font-size:22px;text-decoration:none" class="btn btn-warning">Edit Your Post </a><br><br><br>
+                        @endif
+                    @endif
 
+                  <div class="card-header" style="background-color:blue">
+                        <h1 style="font-weight:bold;font-family:verdana;color:white;font-size:54px;">{{$newsDetails->title  }}</h1><br>
+                  </div>
 
+                  <div class="card">
+                            <div class="card-body">
+                            <img src="{{asset('images/'.$newsDetails->image)}}" width="400px" height="200px" style="border-radius:30px"><br><br>
+                            <span style="color:black;font-weight:bold;font-size:24px">{{ $newsDetails->post }} </span><br><br>
+
+                            <span style="color:black;font-weight:bold;font-size:24px">Article by:</span><span style="font-weight: bold;color:seagreen;font-size:29px">{{ $newsDetails->user->name }}</span>
+
+                    </div>
+                </div>
 
             </div>
 
