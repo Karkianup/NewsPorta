3621e63a73c91a1f66a48282621a2e4e5f461652
Anup Karki <91415516+Karkianup@users.noreply.github.com>
2022-01-15 17:32:10 +0545

    some changes

--- app/Http/Controllers/NewsCategory.php
+++ app/Http/Controllers/NewsCategory.php
@@ -9,15 +9,12 @@ use App\Models\NewsDetail;
 class NewsCategory extends Controller
 {
     function show(Request $req){
-         if(isset($_POST['newsFilter'])){
             $Category=NewsDetail::where('category_id',$req->searchCategory)->get();
+            //   if(count($Category)){
             return view('home_page',[
-            "categories"=>$Category,
-
+              "categories"=>$Category,
             ]);
 
-        }
 
-
-    }
-}
+    // }
+}}
--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -7,49 +7,49 @@
             @csrf
                 <select name="searchCategory">
                      <option selected disabled>filter</option>
-                        {{-- @foreach ($categories as $category)
-                            <option value="{{ $category->category_id }}">{{ $category->id }}</option>
-
-                        @endforeach --}}
-                            <option value="1">1</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->category_id }}">{{ $category->name }}</option>
+                            @endforeach
+                            {{-- <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
-                            <option value="4">4</option>
+                            <option value="4">4</option> --}}
 
                 </select>
                 <input type="submit" value="submit" name="newsFilter">
          </form>
 
-         @if(isset($_POST['newsFilter']))
-         {{-- <div class="container">
-            <div class="row">
-                <div class="col-11">
-                    <div class="card">
-                            <div class="card-body">
-                        @foreach ($newsDetails as $d)
-                            <a href={{ "/news/". $d->id }} style="text-decoration: none">
-                            <h1 style="color:Black;font-weight:bold">{{ $d->title }}</h1> <br>
-                            <img src={{"images/".$d->image  }} width="500px" height="300px"><br><br>
-                            <hr style="width:1000px;height:6px;">
-                            </a>
+         @if (session('message'))
+              <div class="alert alert-primary" style="text-align:center">{{ session('message') }}</div>
+         @endif
 
-                        @endforeach
-                            </div>
+         @if(isset($_POST['newsFilter']))
+            {{-- <div class="container">
+                <div class="row">
+                    <div class="col-11">
+                        <div class="card">
+                                <div class="card-body">
+                            @foreach ($categories as $d)
+                                <a href={{ "/news/". $d->id }} style="text-decoration: none">
+                                <h1 style="color:Black;font-weight:bold">{{ $d->title }}</h1> <br>
+                                <img src={{"images/".$d->image  }} width="500px" height="300px"><br><br>
+                                <hr style="width:1000px;height:6px;">
+                                </a>
+
+                            @endforeach
+                                </div>
+                        </div>
+
                     </div>
 
                 </div>
 
-            </div>
-
-         </div> --}}
-           {{ "My name is Bhagya neupane" }}
+            </div> --}}
 
          @else
 
 
-        @if (session('message'))
-                <div class="alert alert-primary" style="text-align:center">{{ session('message') }}</div>
-        @endif
+
         <div class="container">
             <div class="row">
                 <div class="col-11">
