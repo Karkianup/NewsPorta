7fc18e1300d59e525d84b03d8618ce5467d0fffa
Anup Karki <91415516+Karkianup@users.noreply.github.com>
2022-01-15 19:20:14 +0545

    a

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -16,10 +16,10 @@
          </form>
          {{-- To view using logged in publisher's post --}}
          @if(auth()->check())
-         <form action="" method="POST">
+         <form action="/show/logged/user/article" method="POST">
               @csrf
               <input type="text" name="user_id" value="{{ auth()->user()->id }}">
-                <input type="submit" value="view your article" class="btn btn-success ">
+                <input type="submit" value="view your article" class="btn btn-success" name="searchLoggedUserArticle">
         </form>
         @endif
 
@@ -27,28 +27,28 @@
               <div class="alert alert-primary" style="text-align:center">{{ session('message') }}</div>
          @endif
 
-         @if(isset($_POST['newsFilter']))
-            <div class="container">
-                <div class="row">
-                    <div class="col-11">
-                        <div class="card">
-                                <div class="card-body">
-                            @foreach ($newsArticles as $d)
-                                <a href={{ "/news/". $d->id }} style="text-decoration: none">
-                                <h1 style="color:Black;font-weight:bold">{{ $d->title }}</h1> <br>
-                                <img src={{"images/".$d->image  }} width="500px" height="300px"><br><br>
-                                <hr style="width:1000px;height:6px;">
-                                </a>
+         @if(isset($_POST['newsFilter']) || isset($_POST['searchLoggedUserArticle']))
+                <div class="container">
+                    <div class="row">
+                        <div class="col-11">
+                            <div class="card">
+                                    <div class="card-body">
+                                @foreach ($newsArticles as $d)
+                                    <a href={{ "/news/". $d->id }} style="text-decoration: none">
+                                    <h1 style="color:Black;font-weight:bold">{{ $d->title }}</h1> <br>
+                                    <img src={{"images/".$d->image  }} width="500px" height="300px"><br><br>
+                                    <hr style="width:1000px;height:6px;">
+                                    </a>
 
-                            @endforeach
-                                </div>
+                                @endforeach
+                                    </div>
+                            </div>
+
                         </div>
 
                     </div>
 
                 </div>
-
-            </div>
 
         @else
 
